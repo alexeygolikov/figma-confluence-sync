254dded9d011bff8f288ec4bf6822686474c35cc
alexeygolikov <alexey.an.golikov@gmail.com>
2023-03-18 22:35:45 +0200

    initial commit2

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='figma-confluence-sync',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='alexeygolikov',
+    author_email='',
+    description=''
+)
